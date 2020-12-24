2c0b5a751baa277c31427735a2a3c64461712346
guody <995850944@qq.com>
2020-12-24 15:24:08 +0800

    在test测试一下observation_space得到的值

--- test.py
+++ test.py
@@ -1,21 +1,9 @@
-import dm_control2gym
+import gym
+import numpy as np
 
 
-# Load one task:
-domain = 'fish'
-task = 'downleft'
-# env = suite.load(domain_name="fish", task_name="downleft")
-env = dm_control2gym.make(domain_name=domain, task_name=task)
-# Iterate over a task set:
-# for domain_name, task_name in suite.BENCHMARKING:
-#   env = suite.load(domain_name, task_name)
-#   print(domain_name, task_name)
-# Step through an episode and print out reward, discount and observation.
-# action_spec = env.action_spec()
-# time_step = env.reset()
-# while not time_step.last():
-#   action = np.random.uniform(action_spec.minimum,
-#                              action_spec.maximum,
-#                              size=action_spec.shape)
-#   time_step = env.step(action)
-  # print(time_step.reward, time_step.discount, time_step.observation)
+env = gym.make("CartPole-v1")
+print(env.observation_space)
+print(env.observation_space.shape)
+state_dim = int(np.prod(env.observation_space.shape))
+print(state_dim)
